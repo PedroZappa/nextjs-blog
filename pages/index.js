4527d169def1f804f43a161cbb4f0b74e991d22c
PZ <pedrogzappa@gmail.com>
2022-05-22 21:33:52 +0100

    CSS Modules / Layout Component

--- pages/index.js
+++ pages/index.js
@@ -59,7 +59,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
+          {/* <img src="/vercel.svg" alt="Vercel" className="logo" /> */}
         </a>
       </footer>
 
